dedde8ba879c2867911fd8ef6306428e02719866
Berat ARPA <beratarpa@hotmail.com>
2025-10-06 05:38:07 +0300

    Shared katmanı ve ApiResponse sınıfı eklendi. Ortak kullanımlar için Shared katmanı, başarı veya başarısızlık durumları için ApiResponse sınıfı eklendi.

---
+++ EasyHR.DemoFormApp.Shared/Utilities/Results/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyHR.DemoFormApp.Shared.Utilities.Results
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(T data, string message = "")
+            => new() { Success = true, Message = message, Data = data };
+
+        public static ApiResponse<T> FailResponse(string message)
+            => new() { Success = false, Message = message };
+    }
+}
